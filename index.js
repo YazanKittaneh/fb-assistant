0ac69f54d1c0647d748663577d2ebbd5fd685ce3
yazankittaneh <yazanakittaneh@gmail.com>
2017-01-11 21:07:23 -0600

    changed port to 3000

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ bot.on('message', (payload, reply) => {
   })
 })
 
-http.createServer(bot.middleware()).listen(5000)
+http.createServer(bot.middleware()).listen(3000)
 console.log('Echo bot server running at port 3000.')
