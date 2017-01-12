029b8dd90fba0e2928478fe5559020fb51aa664d
yazankittaneh <yazanakittaneh@gmail.com>
2017-01-11 23:49:32 -0600

    added support to reply when given an image

--- index.js
+++ index.js
@@ -14,16 +14,18 @@ bot.on('error', (err) => {
 
 bot.on('message', (payload, reply) => {
   let text = payload.message.text
-
-  bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) throw err
-
-    reply({ text }, (err) => {
-      if (err) throw err
-
-      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+  if(payload.message.attatchments[0].type !== 'image'){
+    reply({
+      text: "Got the image!"
     })
-  })
+  }
+
 })
+
+
+
+
+
+
 http.createServer(bot.middleware()).listen(process.env.PORT || 3000)
 console.log('Echo bot server running at port 5000.')
