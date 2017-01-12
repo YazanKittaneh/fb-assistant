3482aa33eb7b898167f61c489cfdf130d4f08ed5
yazankittaneh <yazanakittaneh@gmail.com>
2017-01-12 00:42:27 -0600

    removed bad code

--- index.js
+++ index.js
@@ -13,14 +13,17 @@ bot.on('error', (err) => {
 })
 
 bot.on('message', (payload, reply) => {
-  if(payload.message.text !== null){
-    let text = payload.message.text
+  let text = payload.message.text
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
+
     reply({ text }, (err) => {
       if (err) throw err
+
       console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
     })
-  }
-
+  })
 })
 
 
