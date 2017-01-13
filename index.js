7b23eb5d5dcb168d121452ed4b330887cf44ae96
yazankittaneh <yazanakittaneh@gmail.com>
2017-01-13 02:14:20 -0600

    added todo tasks, added support for recieving images

--- index.js
+++ index.js
@@ -15,14 +15,28 @@ bot.on('error', (err) => {
 bot.on('message', (payload, reply) => {
   let text = payload.message.text
   let attachment = payload.message.attachment
-  if (text == null){ //if there isn't any text
+
+  if (attachment.type == "image") {
     reply({
-      text: "there's nothing written ya know"
-    },(err) => {
-      if (err) throw err
+      text: "Got the image!"
     })
   }
-  if (text == "fuck"){
+
+  // if I say something like "remind me about blah"
+    // I should send you a reminder in 2 hours about the thing (and have a snooze button)
+
+  //If I get a calendar event reminder
+    //I should send you a messege about the event
+
+  // If I ask you to put something on my calendar
+    // I should call the events calander and put the event in
+
+  // If I tell you to move an event to a later date/time
+    // grab the most recent event with that name and move it to my desire
+
+  //
+
+  if (text){
     reply({
       text: "alright I got the fuck you're giving me"
     })
